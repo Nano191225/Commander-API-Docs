c3b13d737d29d2f5ad19f7a72535035ef2010f9b
Nano191225 <93137582+Nano191225@users.noreply.github.com>
2025-04-07 21:09:46 +0900

    update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,151 +1,153 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'CAPI Docs',
-  tagline: 'マイクラ統合版のコマンドを拡張',
-  favicon: 'img/favicon.ico',
+    title: "CAPI Docs",
+    tagline: "マイクラ統合版のコマンドを拡張",
+    favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: 'https://capi.un-known.xyz',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: "https://capi.un-known.xyz",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Unknown-Creators-Team', // Usually your GitHub org/user name.
-  projectName: 'Commander-API-Docs', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "Unknown-Creators-Team", // Usually your GitHub org/user name.
+    projectName: "Commander-API-Docs", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'ja',
-    locales: ['ja'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "ja",
+        locales: ["ja"],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Unknown-Creators-Team/Commander-API-Docs/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        // blog: {
-        //   showReadingTime: true,
-        //   feedOptions: {
-        //     type: ['rss', 'atom'],
-        //     xslt: true,
-        //   },
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        //   // Useful options to enforce blogging best practices
-        //   onInlineTags: 'warn',
-        //   onInlineAuthors: 'warn',
-        //   onUntruncatedBlogPosts: 'warn',
-        // },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            "classic",
+            {
+                docs: {
+                    showLastUpdateAuthor: true,
+                    showLastUpdateTime: true,
+                    sidebarPath: "./sidebars.ts",
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        "https://github.com/Unknown-Creators-Team/Commander-API-Docs/tree/main/packages/create-docusaurus/templates/shared/",
+                },
+                // blog: {
+                //   showReadingTime: true,
+                //   feedOptions: {
+                //     type: ['rss', 'atom'],
+                //     xslt: true,
+                //   },
+                //   // Please change this to your repo.
+                //   // Remove this to remove the "edit this page" links.
+                //   editUrl:
+                //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                //   // Useful options to enforce blogging best practices
+                //   onInlineTags: 'warn',
+                //   onInlineAuthors: 'warn',
+                //   onUntruncatedBlogPosts: 'warn',
+                // },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    // image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'CAPI Docs',
-    //   logo: {
-    //     alt: 'My Site Logo',
-    //     src: 'img/logo.svg',
-    //   },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+    themeConfig: {
+        // Replace with your project's social card
+        // image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: "CAPI Docs",
+            //   logo: {
+            //     alt: 'My Site Logo',
+            //     src: 'img/logo.svg',
+            //   },
+            items: [
+                {
+                    type: "docSidebar",
+                    sidebarId: "tutorialSidebar",
+                    position: "left",
+                    label: "Tutorial",
+                },
+                // {to: '/blog', label: 'Blog', position: 'left'},
+                {
+                    href: "https://github.com/facebook/docusaurus",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
         },
-        // {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+        footer: {
+            style: "dark",
+            links: [
+                {
+                    title: "Docs",
+                    items: [
+                        {
+                            label: "マクロ",
+                            to: "/docs/Macro",
+                        },
+                    ],
+                },
+                // {
+                //   title: 'Community',
+                //   items: [
+                //     {
+                //       label: 'Stack Overflow',
+                //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                //     },
+                //     {
+                //       label: 'Discord',
+                //       href: 'https://discordapp.com/invite/docusaurus',
+                //     },
+                //     {
+                //       label: 'X',
+                //       href: 'https://x.com/docusaurus',
+                //     },
+                //   ],
+                // },
+                {
+                    title: "More",
+                    items: [
+                        // {
+                        //   label: 'Blog',
+                        //   to: '/blog',
+                        // },
+                        {
+                            label: "Unknown",
+                            href: "https://un-known.xyz",
+                        },
+                        {
+                            label: "GitHub",
+                            href: "https://github.com/Unknown-Creators-Team",
+                        },
+                        {
+                            label: "Discord",
+                            href: "https://discord.gg/QF3n85dr4P",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © 2025 Unknown. Built with Docusaurus.`,
         },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'マクロ',
-              to: '/docs/Macro',
-            },
-          ],
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-        // {
-        //   title: 'Community',
-        //   items: [
-        //     {
-        //       label: 'Stack Overflow',
-        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //     },
-        //     {
-        //       label: 'Discord',
-        //       href: 'https://discordapp.com/invite/docusaurus',
-        //     },
-        //     {
-        //       label: 'X',
-        //       href: 'https://x.com/docusaurus',
-        //     },
-        //   ],
-        // },
-        {
-          title: 'More',
-          items: [
-            // {
-            //   label: 'Blog',
-            //   to: '/blog',
-            // },
-            {
-                label: 'Unknown',
-                href: 'https://un-known.xyz',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/Unknown-Creators-Team',
-            },
-            {
-                label: 'Discord',
-                href: 'https://discord.gg/QF3n85dr4P',
-            }
-          ],
-        },
-      ],
-      copyright: `Copyright © 2025 Unknown. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
